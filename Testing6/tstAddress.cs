4f5fe4e4963de0736888c011166d5f9dd178f123
Muhammad Umar <P2896780@my365.dmu.ac.uk>
2026-05-06 12:10:57 +0400

    Added tstAddress and clsAddress for Practical 6

---
+++ Testing6/tstAddress.cs
@@ -0,0 +1,20 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing6
+{
+    [TestClass]
+    public class tstAddress
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsAddress AnAddress = new clsAddress();
+
+            //test to see that it exists
+            Assert.IsNotNull(AnAddress);
+        }
+    }
+}
